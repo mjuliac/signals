54c7f6886d39f31de264297e1c708fe0088933fd
Marc Julià Canal <mcanal76@gmail.com>
2025-11-26 18:57:37 +0100

    feat: Add `__del__` methods to unsubscribe from events and rename `user_removed` event to `user_deleted`.

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ class Button:
     def click(self, x, y):
         self.clicked.emit(x, y)
 
+    def __del__(self):
+        self.clicked -= print_click
+
 class Label(Event):
     def __init__(self):
         self.text_changed = Event()
@@ -21,18 +24,27 @@ class Label(Event):
     def set_text(self, text):
         self.text = text
         self.text_changed.emit(self.text)
+
+    def __del__(self):
+        self.text_changed -= print_label
         
 
 class UserDAO:
     def __init__(self):
         self.user_created = Event()
-        self.user_removed = Event()
+        self.user_deleted = Event()
         
     def add(self, user: User):
+        print("Adding user: ", user)
         self.user_created.emit(user)
     
     def remove(self, user: User):
-        self.user_removed.emit(user)
+        print("Deleting user: ", user)
+        self.user_deleted.emit(user)
+
+    def __del__(self):
+        self.user_created -= print_user_created
+        self.user_deleted -= print_user_deleted
 
 
 def print_click(x, y):
@@ -44,8 +56,8 @@ def print_label(text):
 def print_user_created(user):
     print("User created:", user)
 
-def print_user_removed(user):
-    print("User removed:", user)
+def print_user_deleted(user):
+    print("User deleted:", user)
 
 
 def main():
@@ -65,10 +77,13 @@ def main():
     user = User("John", 30, "john@example.com")
     user_dao = UserDAO()
     user_dao.user_created += print_user_created
-    user_dao.user_removed += print_user_removed
+    user_dao.user_deleted += print_user_deleted
     user_dao.add(user)
     user_dao.remove(user)
-
+    del user_dao
+    del user
+    del button
+    del label
 
 if __name__ == "__main__":
     main()
